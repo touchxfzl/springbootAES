c784cf2f51399ec8a06c0217306afee34f7c5e6d
lei <1162799332@qq.com>
2018-09-05 16:42:50 +0800

    AES

---
+++ src/main/java/com/yd/springbootAES/App.java
@@ -0,0 +1,54 @@
+package com.yd.springbootAES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.cxytiandi.encrypt.anno.EnableEncrypt;
+
+//配置main方法
+@SpringBootApplication
+@EnableEncrypt
+public class App {
+	// 这是springloader的配置方式：-javaagent:.\lib\springloaded-1.2.4.RELEASE.jar-noverify
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	// 注入bean 在这里我们使用
+	//
+	// @Bean注入 fastJsonHttpMessageConvert
+	@Bean
+	public HttpMessageConverters fastJsonHttpMessageConverters() {
+		// 1、需要先定义一个 convert 转换消息的对象;
+		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+		// 2、添加fastJson 的配置信息，比如：是否要格式化返回的json数据;
+		FastJsonConfig fastJsonConfig = new FastJsonConfig();
+		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+		// 3、在convert中添加配置信息.
+		fastConverter.setFastJsonConfig(fastJsonConfig);
+		HttpMessageConverter<?> converter = fastConverter;
+		return new HttpMessageConverters(converter);
+	}
+
+	// 配置跨域
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowCredentials(false).maxAge(3600);
+			}
+		};
+
+	}
+
+}
